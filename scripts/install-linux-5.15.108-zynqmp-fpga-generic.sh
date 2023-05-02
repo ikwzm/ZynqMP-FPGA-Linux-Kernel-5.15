ecb0c55a04f33d766a7dc89306daffc54b7e69da
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-05-02 16:36:24 +0900

    [update] BUILD_VERSION to 6

--- scripts/install-linux-5.15.108-zynqmp-fpga-generic.sh
+++ scripts/install-linux-5.15.108-zynqmp-fpga-generic.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 REPO_DIR=$(cd $(dirname $0); cd .. ; pwd)
 KERNEL_VERSION=5.15.108
 LOCAL_VERSION=zynqmp-fpga-generic
-BUILD_VERSION=5
+BUILD_VERSION=6
 
 . "$SCRIPT_DIR/install-command.sh"
